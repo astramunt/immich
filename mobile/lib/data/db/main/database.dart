4a493c5467e67d5c07609b20335fb49e87d85f49
Jason Rasmussen <jason@rasm.me>
2026-09-16 09:21:41 -0400

    feat(mobile): asset face v3

--- mobile/lib/data/db/main/database.dart
+++ mobile/lib/data/db/main/database.dart
@@ -159,7 +159,7 @@ class Drift extends $Drift {
   }
 
   @override
-  int get schemaVersion => 31;
+  int get schemaVersion => 32;
 
   @override
   MigrationStrategy get migration => MigrationStrategy(
@@ -356,6 +356,10 @@ class Drift extends $Drift {
               },
               from30To31: (m, v31) async {
                 await m.createIndex(v31.idxRemoteAssetUploaded);
+              },
+              from31To32: (m, v32) async {
+                // Removed foreign key constraints on asset_face.assetId and asset_face.personId
+                await m.alterTable(TableMigration(v32.assetFaceEntity));
               },
             ),
           ),
